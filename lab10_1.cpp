419f18a80ee1f670faa507e917a1fb12d3c5b745
Foze2547 <sorravichsuk@gmail.com>
2022-12-27 16:42:48 +0700

    10.1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,33 +1,51 @@
-#include<iostream>
-#include<iomanip> //For using setw(), setprecision(), ...
+#include <iostream>
+#include <iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main()
+{
+	double loan, rate, pay, inter, bal;
+	int x = 0;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
+	cin >> pay;
+	// use 'setw' to set width of table and 'left' to set left-alignment
+	// you can change input argument of 'setw()' to see the effect
+	// Try to change from 'left' to 'right' and see the effect
+	cout << setw(13) << left << "EndOfYear#";
+	cout << setw(13) << left << "PrevBalance";
+	cout << setw(13) << left << "Interest";
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+
+	// use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
+	// you can change input argument of 'setprecision()' to see the effect
+	while (loan > 0)
+	{
+
+		cout << fixed << setprecision(2);
+		cout << setw(13) << left << x + 1;
+		cout << setw(13) << left << loan;
+		inter = loan * rate / 100.00;
+		cout << setw(13) << left << inter;
+		cout << setw(13) << left << inter + loan;
+		if (loan + inter - pay < 0)
+		{
+			pay = loan + inter;
+		}
+
+		cout << setw(13) << left << pay;
+		bal = loan + inter - pay;
+		cout << setw(13) << left << bal;
+		cout << "\n";
+
+		loan = bal;
+		x++;
+	}
 	return 0;
 }
