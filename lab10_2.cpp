2fd1988bb13515097b08231175bd07d49f375e19
Foze2547 <sorravichsuk@gmail.com>
2022-12-27 16:20:20 +0700

    10.2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,24 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-//Write the function printO() here
+// Write the function printO() here
+int printO(int x, int y)
+{
+	int i, k;
+	if (x > 0 && y > 0)
+	{
+		for (i = 0; i < x; i++)
+		{
+			for (k = 0; k < y; k++)
+			{
+				cout << "O";
+			}
+			cout << "\n";
+		}
+	}
+	else
+	{
+		cout << "Invalid input";
+	}
+}
 
-int main(){
-	
-	printO(2,2);
+int main()
+{
+
+	printO(2, 2);
 	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
+
+	printO(3, 5);
+	cout << "\n";
+
+	printO(5, 3);
+	cout << "\n";
+
+	printO(0, 3);
+	cout << "\n";
+
+	printO(7, -1);
+	cout << "\n";
+
 	return 0;
 }
